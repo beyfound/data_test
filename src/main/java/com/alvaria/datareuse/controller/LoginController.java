9a610177da4d23e7dc9f9817ba4c78e853a763e7
derek_qiao <derek.qiao@alvaria.com>
2022-09-07 00:09:49 +0800

    Add getUserInfo

--- src/main/java/com/alvaria/datareuse/controller/LoginController.java
+++ src/main/java/com/alvaria/datareuse/controller/LoginController.java
@@ -1,9 +1,11 @@
 package com.alvaria.datareuse.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alvaria.datareuse.Utils.JwtUtil;
 import com.alvaria.datareuse.entity.ResponseResult;
 import com.alvaria.datareuse.entity.UserInfo;
 import com.alvaria.datareuse.service.UserInfoService;
+import com.auth0.jwt.interfaces.Claim;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,12 +28,22 @@ public class LoginController {
             Map<String, String> payload = new HashMap<>();
             payload.put("userId", "2");
             payload.put("username", "admin");
+            payload.put("role", "admin");
             String token = JwtUtil.generateToken(payload);
             tokenMap.put("token", token);
-            tokenMap.put("role", "admin");
-            return new ResponseResult(0,tokenMap,"Login successful");
+
+            return new ResponseResult(0, tokenMap, "Login successful");
         }
 
-        return new ResponseResult(-1,null,"Username or password incorrect");
+        return new ResponseResult(-1, null, "Username or password incorrect");
+    }
+
+    @PostMapping("/user_info")
+    public ResponseResult getUserInfo(@RequestHeader("token") String token) {
+        Map<String, Claim> map = JwtUtil.getPayloadByToken(token);
+        Map<String, String> userInfo = new HashMap<>();
+        userInfo.put("username", map.get("username").asString());
+        userInfo.put("role", map.get("role").asString());
+        return new ResponseResult(0, userInfo, "Login successful");
     }
 }
